03c5559c46b9538ed721d72a732f6f8efef2393c
kamleshmore <112062817+kamleshmore@users.noreply.github.com>
2022-08-25 14:31:03 +0530

    backend code
    backend code

--- backend/src/models/admin.model.js
+++ backend/src/models/admin.model.js
@@ -2,7 +2,19 @@
 
 const db = require("../../config/db.config");
 var Admin = function (Admin) {
-  this.admin_id = Admin.admin_id;
+  this.admin_name = Admin.admin_name;
+  this.username = Admin.username;
+  this.admin_password = Admin.admin_password;
+  this.mobile_number = Admin.mobile_number;
+  this.admin_email = Admin.admin_email;
+  this.user_type = Admin.user_type;
+  this.status = Admin.status;
+  this.subroles = Admin.subroles;
+  this.admin_address = Admin.admin_address;
+  this.address_lat = Admin.address_lat;
+  this.address_long = Admin.address_long;
+  this.dateadded = new Date();
+  this.dateupdated = new Date();
 };
 
 Admin.findAll = function (result) {
