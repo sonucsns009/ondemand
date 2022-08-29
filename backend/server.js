69559e97e1b86119fc1f14127608613f4c317436
kamleshmore <112062817+kamleshmore@users.noreply.github.com>
2022-08-29 15:37:48 +0530

    backend code
    backend code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // multer
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const usersUpload = multer({ dest: "uploads/users" });
+const servicesUpload = multer({ dest: "uploads/servicesdata" });
+const mainCategoryUpload = multer({ dest: "uploads/mainCategory" });
+const mainSubCategoryUpload = multer({ dest: "uploads/mainsubCategory" });
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -48,9 +51,53 @@ app.use("/api/v1/serviceSubCategory", serviceSubCategoryRoutes);
 
 // file upload starts
 
-app.post("/upload_files", upload.array("files"), uploadFiles);
+//  users
+app.post(
+  "/api/v1/users/upload_files",
+  usersUpload.array("files"),
+  usersUploadFiles
+);
 
-function uploadFiles(req, res) {
+function usersUploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ message: "Successfully uploaded files" });
+}
+
+//  services
+app.post(
+  "/api/v1/services/upload_files",
+  servicesUpload.array("files"),
+  servicesUploadFiles
+);
+
+function servicesUploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ message: "Successfully uploaded files" });
+}
+
+//  main category
+app.post(
+  "/api/v1/mainCategory/upload_files",
+  mainCategoryUpload.array("files"),
+  mainCategoryUploadFiles
+);
+
+function mainCategoryUploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ message: "Successfully uploaded files" });
+}
+
+//  main sub category
+app.post(
+  "/api/v1/mainSubCategory/upload_files",
+  mainSubCategoryUpload.array("files"),
+  mainSubCategoryUploadFiles
+);
+
+function mainSubCategoryUploadFiles(req, res) {
   console.log(req.body);
   console.log(req.files);
   res.json({ message: "Successfully uploaded files" });
