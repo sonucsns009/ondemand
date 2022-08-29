25486c488a4078f5fbede39ddd96223e0e5f6307
kamleshmore <112062817+kamleshmore@users.noreply.github.com>
2022-08-29 14:49:56 +0530

    changes
    changes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+// multer
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -42,6 +45,18 @@ app.use("/api/v1/mainSubCategory", mainSubCategoryRoutes);
 app.use("/api/v1/services", servicesRoutes);
 app.use("/api/v1/serviceCategory", serviceCategoryRoutes);
 app.use("/api/v1/serviceSubCategory", serviceSubCategoryRoutes);
+
+// file upload starts
+
+app.post("/upload_files", upload.array("files"), uploadFiles);
+
+function uploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ message: "Successfully uploaded files" });
+}
+
+// file upload ends
 
 app.use((req, res, next) => {
   res.status(404).send("page not found");
