a4441188fefc5135acc76419db6c29a0b9dfeb07
sagar-surse <sagarsurse2@gmail.com>
2022-09-06 19:25:45 +0530

    frontend changes
    frontend changes

---
+++ frontend/src/pages/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logout = () => {
+    return (
+        <div>
+            <h1>Analytics page</h1>
+        </div>
+    );
+};
+
+export default Logout;
---
+++ frontend/src/pages/Maincategory.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Maincategory = () => {
+    return (
+    
+    
+             <div className="container">
+                <div className="row">
+                 <div className="col-sm-12">
+                    <div className="card-body">
+                      <h3>Main Category</h3>
+                    </div>
+                    </div>                    
+                </div>
+                </div>            
+            );
+};
+
+export default Maincategory;
