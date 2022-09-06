38364a485cdbec0b40184f7df1449fa7565d3f19
sagar-surse <sagarsurse2@gmail.com>
2022-09-06 19:08:53 +0530

    dashboard

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 function Login() {
   const [username,setUsername]=useState("");
-    const [usernameerror,setUsernameError]=useState(false);
+  const [usernameerror,setUsernameError]=useState(false);
 
     const [password,setPassword]=useState("");
     const [passworderror,setPasswordError]=useState(false);
@@ -54,8 +54,9 @@ function Login() {
                 console.warn(respone.data);
                 if(respone.data.length > 0){
                   setPasswordError(false)
+                  
                   localStorage.setItem('user', JSON.stringify(respone));
-                  //navigate("/dashboard"); 
+                  //navigate("/dashboard");
                   return respone.json();
                 }
                 else {
@@ -68,26 +69,33 @@ function Login() {
           setPassword("")
       }   
   }
-    return (
+    return (  
+      <div className="col-sm-12">
+      
       <div className="hold-transition login-page">
         <div className="login-box text-center">
-          <div className="login-logo">
-             <img src={Logo} />           
-            {/* <a href="../../index2.html"><b>On Demand System</b>CSNS</a> */}
-          </div>
+        <div clasName="container">    
+          <div className="row">
+                     
           {/* /.login-logo */}
           
-          <div className="card">
-            <form name="frm_login" id="frm_login" onSubmit={handleSubmit}>
+          
+              <div className="col-sm-4 p-0 card center">
+              <div className="card bg-primary bg-color">
+            <div className="login-logo">
+             <img className="blur-up lazyloaded" alt="" src={Logo} style={{ height: 100 }} />           
+            {/* <a href="../../index2.html"><b>On Demand System</b>CSNS</a> */}
+          </div>
+          </div>
+              <div className="card">
+               <form name="frm_login" id="frm_login" onSubmit={handleSubmit}>
 
               <div className="card-body login-card-body">
                 <p className="login-box-msg">Sign in to start your session</p>
                   <div className="input-group mb-3">
                     <input type="email" className="form-control" placeholder="Email" value={username} id="username" autoComplete="off" onChange={(e)=>setUsername(e.target.value)}/>
                     <div className="input-group-append">
-                      <div className="input-group-text">
-                        <span className="fas fa-envelope" />
-                      </div>
+                      
                     </div>
                     {usernameerror&& <div className="error-msg" style={{color:"red"}}>{usernameerror}</div>}
 
@@ -95,15 +103,13 @@ function Login() {
                   <div className="input-group mb-3">
                     <input type="password" className="form-control" placeholder="Password" value={password}  onChange={(e)=>setPassword(e.target.value)}/>
                     <div className="input-group-append">
-                      <div className="input-group-text">
-                        <span className="fas fa-lock" />
-                      </div>
+                      
                     </div>
                     {passworderror&& <div className="error-msg" style={{color:"red"}}>{passworderror}</div>}
 
                   </div>
                   <div className="row">
-                    <div className="col-8">
+                    <div className="col-4">
                       <div className="icheck-primary">
                         <input type="checkbox" id="remember" />
                         <label htmlFor="remember">
@@ -111,12 +117,15 @@ function Login() {
                         </label>
                       </div>
                     </div>
+                    </div>
                     {/* /.col */}
+                    <div className="row">
                     <div className="col-4">
                       <button type="submit" className="btn btn-primary btn-block">Sign In</button>
                     </div>
                     {/* /.col */}
                   </div>
+                  <div className="row">
                 <div className="social-auth-links text-center mb-3">
                   <p>- OR -</p>
                   {/* <a href="#" className="btn btn-block btn-primary">
@@ -126,20 +135,28 @@ function Login() {
                   <i className="fab fa-google-plus mr-2" /> Sign in using Google+
                 </a> */}
                 </div>
+                </div>
                 {/* /.social-auth-links */}
+                <div className="row">
                 <p className="mb-1">
                   <a href="forgot-password.html">I forgot my password</a>
                 </p>
+                </div>
+                <div className="row">
                 <p className="mb-0">
                   <a href="register.html" className="text-center">Register a new membership</a>
                 </p>
               </div>
+              </div>
               {/* /.login-card-body */}
             </form>
+            </div>
+            </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
-
     );
 }
 
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,75 +1,101 @@
-import React from 'react';
-import { Component } from 'react';
-//import { Link } from "react-router-dom";
-class Sidebar extends Component {
-    
-    render() {
+import React, { useState } from 'react';
+import {
+    FaTh,
+    FaBars,
+    FaUserAlt,
+    FaRegChartBar,
+    FaCommentAlt,
+    FaShoppingBag,
+    FaThList,
+    FaPowerOff
 
+}from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import Logo from '../images/CSNS_logo2.png';
+
+const Sidebar = ({children}) => {
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+    const menuItem=[
+    {
+            path:"/",
+            name:"Login",
+            
+    },
+    {
+        path:"/registration",
+        name:"Registration",
         
-        return (
-            <div>
-                   <aside className="main-sidebar sidebar-dark-primary elevation-4">
-                            {/* Brand Logo */}
-                            <a href="index3.html" className="brand-link">
-                            <img src={require('./dist/CSNS_logo2.png')} alt="AdminLTE Logo" className="brand-image img-circle elevation-3"  style={{height:"150px", marginLeft:"20px",width:"170px"}}/>
-                            <span className="brand-text font-weight-light"> &nbsp;</span>
-                            </a>
-                            {/* Sidebar */}
-                        <div className="sidebar">
-                            {/* Sidebar user panel (optional) */}
-                            <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                                <div className="image">
-                                    <img src="dist/img/user2-160x160.jpg"  alt="User Image" />
-                                </div>
-                                <div className="info">
-                                    <a href="#" className="d-block">Alexander Pierce</a>
-                                </div>
-                            </div>
-                            {/* SidebarSearch Form */}
-                            <div className="form-inline">
-                                <div className="input-group" data-widget="sidebar-search">
-                                     <input className="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
-                                    <div className="input-group-append">
-                                        <button className="btn btn-sidebar">
-                                        <i className="fas fa-search fa-fw" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* Sidebar Menu */}
-                            <nav className="mt-2">
-                                <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                                {/* Add icons to the links using the .nav-icon class
-                                with font-awesome or any other icon font library */}
-                                    <li className="nav-item">
-                                        <a href="./index.html" className="nav-link">
-                                        <i className="far fa-circle nav-icon" />
-                                        <p>Dashboard v1</p>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="./index.html" className="nav-link">
-                                        <i className="far fa-circle nav-icon" />
-                                        <p>Main Category</p>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="./index.html" className="nav-link">
-                                        <i className="far fa-circle nav-icon" />
-                                        <p>Main Sub Category</p>
-                                        </a>
-                                    </li>
-                               
-                                </ul>
-                            </nav>
-                            {/* /.sidebar-menu */}
-                        </div>
-                            {/* /.sidebar */}
-                    </aside>
-                       
-            </div>
-        );
-    }
-}
+    },
+    {
+        path:"/dashboard",
+        name:"Dashboard",
+        icon:<FaTh/>
+    },
+    {
+        path:"/about",
+        name:"About",
+        icon:<FaUserAlt/>
+    },
+    {
+        path:"/maincategory",
+        name:"Maincategory",
+        icon:<FaUserAlt/>
+    },
+    {
+        path:"/mainsubcategory",
+        name:"Mainsubcategory",
+        icon:<FaUserAlt/>
+    },
+    {
+        path:"/analytics",
+        name:"Analytics",
+        icon:<FaRegChartBar/>
+    },
+    {
+        path:"/comment",
+        name:"Comment",
+        icon:<FaCommentAlt/>
+    },
+    {
+        path:"/product",
+        name:"Product",
+        icon:<FaShoppingBag/>
+    },
+    {
+        path:"/productList",
+        name:"Product List",
+        icon:<FaThList/>
+    },
+    {
+        path:"/logout",
+        name:"Logout",
+        icon:<FaPowerOff/>
+    },
+    
+]
+    return (
+        <div>
+           <div style={{width: isOpen ? "" : "50px"}} className="sidebar">
+               <div className="top_section">
+                 <img src={Logo} alt="" style={{display: isOpen ? "block" : "none"}} className="logo"/>
+                   {/*<h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>*/}
+                   <div style={{marginLeft: isOpen ? "" : "0px"}} className="bars">
+                       <FaBars onClick={toggle}/>
+                   </div>
+               </div>
+               {
+                   menuItem.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                           <div className="icon">{item.icon}</div>
+                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                       </NavLink>
+                   ))
+               }
+           </div>
+           <main>{children}</main>
+        </div>
+    );
+};
 
 export default Sidebar;
